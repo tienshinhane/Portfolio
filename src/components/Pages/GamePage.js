e713d93145c1c88ee6675b3860a9057d59e50e53
edgespeargames <73969835+edgespeargames@users.noreply.github.com>
2021-03-28 21:00:09 +0100

    Added a back button on WebGL page

--- src/components/Pages/GamePage.js
+++ src/components/Pages/GamePage.js
@@ -1,4 +1,6 @@
 import Unity, { UnityContent } from "react-unity-webgl";
+import { Link } from "react-router-dom";
+import { Link as scrollLink } from "react-scroll";
 import "./GamePage.css";
 
 const GamePage = (props) => {
@@ -13,6 +15,11 @@ const GamePage = (props) => {
         <div className="title">{props.location.state.title}</div>
         <div className="unity-container">
           <Unity unityContent={myUnityContent} className="unity-content" />
+        </div>
+        <div className="button-container">
+          <Link to="/" className="play-link-game">
+            Back
+          </Link>
         </div>
       </div>
     </>
--- src/components/Projects/PlayContent2.js
+++
@@ -1,5 +0,0 @@
-import GamePage from "../Unity/GamePage";
-
-export const openPopupboxPlay = (title, link1, link2) => {
-  <GamePage title={title} link1={link1} link2={link2} />;
-};
